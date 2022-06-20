5bdb528dd838e6f04704f9fc4abd1b52aec0e05f
huangkaiqiao <huangkaiqiao@gmail.com>
2022-06-20 14:29:31 +0800

    Send to web3storage 90%

--- src/home/index.js
+++ src/home/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import './index.css';
 import { Alert, Button, Card, Form, Spinner, ListGroup } from "react-bootstrap";
+import { Web3Storage } from "web3.storage";
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: 'prepare'
+      status: 'prepare',
+      cid: '',
     };
 
+    this.fileInput = React.createRef()
     this.handleBack = this.handleBack.bind(this)
     this.handleSend = this.handleSend.bind(this)
     this.handleStop = this.handleStop.bind(this)
   }
 
   componentDidMount() {
-    console.log(process.env.NODE_ENV, process.env.REACT_APP_API_TOKEN)
+    // console.log(process.env.NODE_ENV, process.env.REACT_APP_API_TOKEN)
   }
 
   handleBack(e) {
@@ -25,19 +28,35 @@ export default class Home extends React.Component {
     })
   }
 
+  handleSelect(e) {
+    console.log(e)
+  }
+
   handleSend(e) {
     console.log('kanaloa');
     e.preventDefault();
-    // this.state.status = 'pending';
-    // let $ = this;
-    this.setState({
-      status: 'pending'
+
+    // this.setState({
+    //   status: 'pending'
+    // })
+    // setTimeout(()=>{
+    //   this.setState({
+    //     status: 'finished'
+    //   })
+    // }, 5000)
+    const token = process.env.REACT_APP_API_TOKEN
+    const storage = new Web3Storage({ token })
+    // const data = new FormData();
+    // data.append('file', this.fileInput.current.files[0]);
+    const files = this.fileInput.current.files;
+    console.log(`Uploading ${files.length} files`)
+    this.setState({ status: 'pending' })
+    storage.put(files).then(cid => {
+      console.log('Content added with CID:', cid)
+      this.setState({ cid: cid, status: 'finished' })
+    }).catch(err => {
+      console.err(err)
     })
-    setTimeout(()=>{
-      this.setState({
-        status: 'finished'
-      })
-    }, 5000)
   }
 
   handleStop(e) {
@@ -59,7 +78,7 @@ export default class Home extends React.Component {
             {this.state.status === 'prepare' && <Form>
                 <Form.Group controlId="formFile" className="mb-3">
                   {/* <Form.Label>Default file input example</Form.Label> */}
-                  <Form.Control type="file" />
+                  <Form.Control type="file" onChange={this.handleSelect} ref={this.fileInput}/>
                 </Form.Group>
                 <Button variant="primary" onClick={this.handleSend}>Send via IPFS</Button>
             </Form>}
@@ -77,8 +96,11 @@ export default class Home extends React.Component {
               </Alert>
               <ListGroup as="ul">
                 <ListGroup.Item as="li" style={{border: 'none', padding: '1rem 0.5rem 0rem'}}>
-                  <a style={{fontSize: '14px'}} href="https://bafybeidegclbs3ijzbklnelqw7zlshfy3ph44avkw3rhxm2u7ucjizsauq.ipfs.dweb.link">
+                  {/* <a style={{fontSize: '14px'}} href="https://bafybeidegclbs3ijzbklnelqw7zlshfy3ph44avkw3rhxm2u7ucjizsauq.ipfs.dweb.link">
                     https://bafybeidegclbs3ijzbklnelqw7zlshfy3ph44avkw3rhxm2u7ucjizsauq.ipfs.dweb.link
+                  </a> */}
+                  <a style={{fontSize: '14px'}} href={"https://" + this.state.cid + ".ipfs.dweb.link"}>
+                    https://{this.state.cid}.ipfs.dweb.link
                   </a>
                 </ListGroup.Item>
               </ListGroup>
